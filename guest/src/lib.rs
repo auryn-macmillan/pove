a5bdb1befd3c52d263c1fff0968ca5b9a23f39af
Auryn Macmillan <hi@auryn.xyz>
2024-08-06 23:01:31 -0400

    use local jolt-toolchain

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,17 +1,9 @@
 #![no_main]
 
-use fhe::bfv;
-use fhe_traits::Serialize;
-use std::{sync::Arc, usize};
+use fhe::bfv::BfvParameters;
+use fhe_traits::Deserialize;
 
 #[jolt::provable]
-fn generate_params(degree: usize, plaintext_modulus: u64, moduli: Vec<u64>) -> Vec<u8> {
-    let params: Arc<bfv::BfvParameters> = bfv::BfvParametersBuilder::new()
-        .set_degree(degree)
-        .set_plaintext_modulus(plaintext_modulus)
-        .set_moduli(&moduli)
-        .build_arc()
-        .unwrap();
-
-    params.to_bytes()
+fn deserialize_params(param_bytes: Vec<u8>) {
+    BfvParameters::try_deserialize(&param_bytes).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use fhe::bfv::BfvParameters;
-use fhe_traits::Deserialize;
+use fhe::bfv::{BfvParameters, BfvParametersBuilder};
+use fhe_traits::Serialize;
+use std::{sync::Arc, usize};
 
 pub fn main() {
     let degree: usize = 1024;
     let plaintext_modulus: u64 = 65537;
     let moduli: Vec<u64> = vec![1152921504606584833];
 
-    let (prove_params, verify_params) = guest::build_generate_params();
+    let params: Arc<BfvParameters> = BfvParametersBuilder::new()
+        .set_degree(degree)
+        .set_plaintext_modulus(plaintext_modulus)
+        .set_moduli(&moduli)
+        .build_arc()
+        .unwrap();
 
-    let (output, proof) = prove_params(degree, plaintext_modulus, moduli);
+    let (prove_params, verify_params) = guest::build_deserialize_params();
+
+    let (_return, proof) = prove_params(params.to_bytes());
     let is_valid = verify_params(proof);
 
-    let params = BfvParameters::try_deserialize(&output).unwrap();
-
-    println!("output: {:?}", params);
+    println!("output:\n{}\n{}\n{:?}", degree, plaintext_modulus, moduli);
     println!("valid: {}", is_valid);
 }
