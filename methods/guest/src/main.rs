61f3f86457bd7267c3cd3408f7122bb29db0f754
Hamza Khalid <hamzakhalid829@gmail.com>
2024-08-09 00:16:25 +0500

    CT generation in guest

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,24 +1,41 @@
-use fhe::bfv::{BfvParameters, BfvParametersBuilder};
-use fhe_traits::Serialize;
+use fhe::bfv::{self, BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
+use fhe_traits::{
+    Deserialize, DeserializeParametrized, FheDecoder, FheEncoder, FheEncrypter, Serialize,
+};
 use risc0_zkvm::guest::env;
 use std::sync::Arc;
+use rand::thread_rng;
 
 fn main() {
-    let degree: u64 = env::read();
-    let plaintext_modulus: u64 = env::read();
-    let moduli: Vec<u64> = env::read();
+    // read the public input from the journal
+    let params_bytes: Vec<u8> = env::read();
+    let pk_bytes: Vec<u8> = env::read();
+    let input: u64 = env::read();
 
-    let params: Arc<BfvParameters> = BfvParametersBuilder::new()
-        .set_degree(degree as usize)
-        .set_plaintext_modulus(plaintext_modulus)
-        .set_moduli(&moduli)
-        .build_arc()
-        .unwrap();
+    let params: Arc<BfvParameters> =
+        Arc::new(BfvParameters::try_deserialize(&params_bytes).unwrap());
+    println!(
+        "Degree: {}, Plaintext: {}, Moduli: {:?}",
+        params.degree(),
+        params.plaintext(),
+        params.moduli()
+    );
 
-    let serialized_params: Vec<u8> = params.to_bytes();
+    let pk: PublicKey = PublicKey::from_bytes(&pk_bytes, &params).unwrap();
 
-    // let vectorized_params: Vec<u8> = serialized_params;
+    // Check if inputs are between 0 and 10
+    if input < 0 || input > 10 {
+        panic!("Input must be between 0 and 10");
+    }
+
+    println!("Public key: {:?}", pk);
+    println!("Inputs: {:?}", input);
+
+    let mut rng = thread_rng();
+
+    let pt = Plaintext::try_encode(&vec![input], Encoding::poly(), &params).unwrap();
+    let ct: Ciphertext = pk.try_encrypt(&pt, &mut rng).unwrap();
 
     // write public output to the journal
-    env::commit(&serialized_params);
+    env::commit(&ct.to_bytes());
 }
