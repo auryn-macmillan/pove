c257296e06cf5188c43c8ed62d482b3c41b58f06
Auryn Macmillan <hi@auryn.xyz>
2024-08-07 22:11:09 -0400

    first attempt at SP1
    Attempting to prove correct execution of a toy program using the fhe.rs library. The `lib.rs` file simply builds the BFV parameters based on the given inputs and returns the serialized result.
    
    `--execute` runs successfully, but `--prove` fails with "Killed". I have a feeling my little laptop is just running out of ram or something.
    
    Example output:
    ```sh
    degree: 1024
    plaintext_modulus: 65537
    moduli: [1152921504606584833]
    2024-08-08T01:48:32.842947Z  INFO prove_core: clk = 0 pc = 0x202080
    2024-08-08T01:48:33.902374Z  INFO prove_core: clk = 10000000 pc = 0x24e170
    2024-08-08T01:48:34.723756Z  INFO prove_core: clk = 20000000 pc = 0x24daf0
    2024-08-08T01:48:35.551875Z  INFO prove_core: clk = 30000000 pc = 0x24db4c
    2024-08-08T01:48:36.368141Z  INFO prove_core: clk = 40000000 pc = 0x24db30
    2024-08-08T01:48:37.080708Z  INFO prove_core: clk = 50000000 pc = 0x23c7b0
    2024-08-08T01:48:37.750334Z  INFO prove_core: clk = 60000000 pc = 0x2353e8
    2024-08-08T01:48:38.236295Z  INFO prove_core: clk = 0 pc = 0x202080
    2024-08-08T01:48:38.436997Z  INFO prove_core: clk = 70000000 pc = 0x2249f4
    2024-08-08T01:48:39.110055Z  INFO prove_core: clk = 80000000 pc = 0x24db54
    2024-08-08T01:48:43.174070Z  INFO prove_core: clk = 10000000 pc = 0x24e170
    2024-08-08T01:48:48.092486Z  INFO prove_core: clk = 20000000 pc = 0x24daf0
    2024-08-08T01:48:52.829958Z  INFO prove_core: clk = 30000000 pc = 0x24db4c
    2024-08-08T01:48:57.897164Z  INFO prove_core: clk = 40000000 pc = 0x24db30
    2024-08-08T01:49:02.713632Z  INFO prove_core: clk = 50000000 pc = 0x23c7b0
    2024-08-08T01:49:07.083635Z  INFO prove_core: clk = 60000000 pc = 0x2353e8
    Killed
    ```

--- guest/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-
-use fhe::bfv::BfvParameters;
-use fhe_traits::Deserialize;
-
-#[jolt::provable]
-fn deserialize_params(param_bytes: Vec<u8>) {
-    BfvParameters::try_deserialize(&param_bytes).unwrap();
-}
