f38b1fe03905b1f4361c74927f36885b7355b9df
Auryn Macmillan <hi@auryn.xyz>
2024-08-08 22:18:06 -0400

    vector of inputs

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,16 +1,14 @@
-use fhe::bfv::{self, BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
-use fhe_traits::{
-    Deserialize, DeserializeParametrized, FheDecoder, FheEncoder, FheEncrypter, Serialize,
-};
+use fhe::bfv::{BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey};
+use fhe_traits::{Deserialize, DeserializeParametrized, FheEncoder, FheEncrypter, Serialize};
+use rand::thread_rng;
 use risc0_zkvm::guest::env;
 use std::sync::Arc;
-use rand::thread_rng;
 
 fn main() {
     // read the public input from the journal
     let params_bytes: Vec<u8> = env::read();
     let pk_bytes: Vec<u8> = env::read();
-    let input: u64 = env::read();
+    let input: Vec<u64> = env::read();
 
     let params: Arc<BfvParameters> =
         Arc::new(BfvParameters::try_deserialize(&params_bytes).unwrap());
@@ -23,9 +21,10 @@ fn main() {
 
     let pk: PublicKey = PublicKey::from_bytes(&pk_bytes, &params).unwrap();
 
-    // Check if inputs are between 0 and 10
-    if input < 0 || input > 10 {
-        panic!("Input must be between 0 and 10");
+    // checksum
+    let sum: u64 = input.iter().sum();
+    if sum > 1 {
+        panic!("Too many votes cast");
     }
 
     println!("Public key: {:?}", pk);
@@ -33,7 +32,7 @@ fn main() {
 
     let mut rng = thread_rng();
 
-    let pt = Plaintext::try_encode(&vec![input], Encoding::poly(), &params).unwrap();
+    let pt = Plaintext::try_encode(&input, Encoding::poly(), &params).unwrap();
     let ct: Ciphertext = pk.try_encrypt(&pt, &mut rng).unwrap();
 
     // write public output to the journal
