11b225748b5db24f8dc13ca545b56ef3e67b70d6
Auryn Macmillan <hi@auryn.xyz>
2024-08-09 15:56:24 -0400

    implement toy custom_getrandom

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -2,10 +2,12 @@
 
 use fhe::bfv;
 use fhe_traits::Serialize;
-use std::{sync::Arc, usize};
+use getrandom::register_custom_getrandom;
+use std::sync::Arc;
 
 #[jolt::provable]
 fn generate_params(degree: usize, plaintext_modulus: u64, moduli: Vec<u64>) -> Vec<u8> {
+    register_custom_getrandom!(my_custom_getrandom);
     let params: Arc<bfv::BfvParameters> = bfv::BfvParametersBuilder::new()
         .set_degree(degree)
         .set_plaintext_modulus(plaintext_modulus)
@@ -14,4 +16,11 @@ fn generate_params(degree: usize, plaintext_modulus: u64, moduli: Vec<u64>) -> V
         .unwrap();
 
     params.to_bytes()
+}
+
+fn my_custom_getrandom(dest: &mut [u8]) -> Result<(), getrandom::Error> {
+    for byte in dest {
+        *byte = 42; // Replace 42 with your static number
+    }
+    Ok(())
 }
