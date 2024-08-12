791dcf5472ca95863528914f6cece2e2aabec549
Hamza Khalid <hamzakhalid829@gmail.com>
2024-08-12 18:31:46 +0500

    Register custom getrandom

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,14 +1,11 @@
 #![no_main]
-
-use fhe::bfv;
+use fhe::bfv::BfvParametersBuilder;
 use fhe_traits::Serialize;
-use getrandom::register_custom_getrandom;
-use std::sync::Arc;
+
 
 #[jolt::provable]
 fn generate_params(degree: usize, plaintext_modulus: u64, moduli: Vec<u64>) -> Vec<u8> {
-    register_custom_getrandom!(my_custom_getrandom);
-    let params: Arc<bfv::BfvParameters> = bfv::BfvParametersBuilder::new()
+    let params = BfvParametersBuilder::new()
         .set_degree(degree)
         .set_plaintext_modulus(plaintext_modulus)
         .set_moduli(&moduli)
@@ -18,9 +15,9 @@ fn generate_params(degree: usize, plaintext_modulus: u64, moduli: Vec<u64>) -> V
     params.to_bytes()
 }
 
-fn my_custom_getrandom(dest: &mut [u8]) -> Result<(), getrandom::Error> {
+pub fn my_custom_getrandom(dest: &mut [u8]) -> Result<(), getrandom::Error> {
     for byte in dest {
-        *byte = 42; // Replace 42 with your static number
+        *byte = 42;
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use fhe::bfv::BfvParameters;
 use fhe_traits::Deserialize;
+use getrandom::register_custom_getrandom;
 
+register_custom_getrandom!(guest::my_custom_getrandom);
 pub fn main() {
     let degree: usize = 1024;
     let plaintext_modulus: u64 = 65537;
     let moduli: Vec<u64> = vec![1152921504606584833];
 
+    println!("Generating parameters for BFV scheme with degree = {}, plaintext_modulus = {}, and moduli = {:?}", degree, plaintext_modulus, moduli);
+
     let (prove_params, verify_params) = guest::build_generate_params();
+
+    println!("Proving and verifying parameters...");
 
     let (output, proof) = prove_params(degree, plaintext_modulus, moduli);
     let is_valid = verify_params(proof);
